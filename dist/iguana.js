1659f0cde7bc45437565b33e933a0663cd12183b
Nate Brustein <nate@pedago.com>
2014-03-11 15:12:25 -0400

    fix to lazy loading stuff

--- dist/iguana.js
+++ dist/iguana.js
@@ -591,10 +591,10 @@ angular.module('Iguana')
         
     }]);
 angular.module('Iguana')
-.factory('Iguana.SingleCollectionInheritance', function(){
-        
+    .factory('Iguana.SingleCollectionInheritance', function() {
+
         return {
-            
+
             included: function(Iguana) {
                 // if the class has an alias, but the type property is 
                 // not set, then set it to the alias.
@@ -604,62 +604,75 @@ angular.module('Iguana')
                         this[sciProperty] = this.constructor.alias();
                     }
                 });
-                
+
                 Iguana.setCallback('before', 'copyAttrsOnInitialize', function() {
                     var attrs = this.$$sourceAttrs;
                     if (!attrs || !attrs.$instantiatedWithNew) {
                         throw new Error("Iguana classes must be instantiated with MyKlass.new() rather that new MyKlass()");
                     }
-                    
+
                     delete attrs.$instantiatedWithNew;
                 });
             },
-            
+
             classMixin: {
-                
+
                 sciProperty: '__iguana_type',
-                
+
                 setSciProperty: function(prop) {
-                    this.extend({sciProperty: prop});
+                    this.extend({
+                        sciProperty: prop
+                    });
                 },
 
                 new: function(attrs, raiseOnFailure) {
                     //clone the provided attrs object
                     attrs = angular.extend({}, attrs);
-                    
+
                     if (raiseOnFailure !== false) {
                         raiseOnFailure = true;
                     }
                     if (attrs === undefined) {
                         attrs = {};
                     }
-                    
-                    if (typeof attrs !== 'object' || Object.prototype.toString.call( attrs ) === '[object Array]') {
-                        throw new Error("Expecting to instantiate Iguana class with object, got '"+attrs+"'");
+
+                    if (typeof attrs !== 'object' || Object.prototype.toString.call(attrs) === '[object Array]') {
+                        throw new Error("Expecting to instantiate Iguana class with object, got '" + attrs + "'");
                     }
-                    
+
                     //Ensure that all instances are created with Iguana.new rather than 'new Iguana'
                     //See after copyAttrs callback above
                     attrs.$instantiatedWithNew = true;
-                    
+
                     var instance;
+
+                    // Since the lazy-loading relies on sciProperty, it can only work on subclasses. I guess this makes
+                    // sense, since we'll always be loading things from the db with SomeItem.show() or 
+                    // whatever.
                     if (!attrs.hasOwnProperty(this.sciProperty)) {
                         return new this(attrs);
-                    }
-                    else if (attrs[this.sciProperty] && attrs[this.sciProperty] === this.alias()) {
+                    } else if (attrs[this.sciProperty] && attrs[this.sciProperty] === this.alias()) {
                         instance = new this(attrs);
                     } else {
-                        for (var i = 0; i < this.subclasses.length; i++) {
-                            var subclass = this.subclasses[i];
-                            instance = subclass.new(attrs, false);
-                            if (instance) { break; }
+                        var klass
+                        try {
+                            klass = this.getAliasedKlass(attrs[this.sciProperty]);
+                        } catch (e) {}
+
+                        if (klass && !klass.inheritsFrom(this)) {
+                            throw new Error('Cannot instantiate because class "' + klass.alias() + '" does not inherit from "' + this.alias() + '."');
                         }
+
+                        if (klass) {
+                            instance = klass.new(attrs, false);
+                        }
+
                     }
 
                     if (instance) {
                         return instance;
                     } else if (raiseOnFailure) {
-                        throw new Error('No class could be found for '+this.sciProperty+'="'+attrs[this.sciProperty]+'".');
+                        throw new Error('No class could be found for ' + this.sciProperty + '="' + attrs[this.sciProperty] + '".');
                     }
                 }
             }
--- spec/single_collection_inheritance_spec.js
+++ spec/single_collection_inheritance_spec.js
@@ -1,9 +1,9 @@
 'use strict';
 
 describe('Iguana.SingleCollectionInheritance', function() {
-    
+
     var Iguana, Item;
-    
+
     beforeEach(function() {
         module('Iguana');
 
@@ -13,10 +13,10 @@ describe('Iguana.SingleCollectionInheritance', function() {
         });
 
     });
-    
+
     describe('new', function() {
         var ItemType1, ItemType2;
-        
+
         beforeEach(function() {
 
             // # Single Collection Inheritance
@@ -36,58 +36,66 @@ describe('Iguana.SingleCollectionInheritance', function() {
                     this.alias('item');
                     this.setCollection('items');
                 });
-                
+
                 ItemType1 = Item.subclass(function() {
                     this.alias('item_type_1');
                 });
-                
+
                 ItemType2 = Item.subclass(function() {
                     this.alias('item_type_2');
                 });
             });
 
         });
-        
+
         // Now that you have called alias(), when a document 
         // is loaded up over the api, it will be instantiated
         // as instance of the appropriate class.
         it('should create an instance of a subclass if the type property matches the alias', function() {
             // ...
             //There are two items in the database
-            var items = [
-                {id: 1, __iguana_type: 'item_type_1'},
-                {id: 2, __iguana_type: 'item_type_2'}];
-            
+            var items = [{
+                id: 1,
+                __iguana_type: 'item_type_1'
+            }, {
+                id: 2,
+                __iguana_type: 'item_type_2'
+            }];
+
             //Mocking out the adapter to load up the first item.
-            Item.adapter().expect('show', 'items', 1, {result: [items[0]]});
-            Item.show(1).then(function(response){
+            Item.adapter().expect('show', 'items', 1, {
+                result: [items[0]]
+            });
+            Item.show(1).then(function(response) {
                 var item = response.result;
-                
+
                 //Since the __iguana_type is 'item_type_1', the result is 
                 //an instance of ItemType1
                 expect(item.constructor).toBe(ItemType1);
             });
-            
+
             //Mocking out the adapter to load up the second item.
-            Item.adapter().expect('show', 'items', 2, {result: [items[1]]});
-            Item.show(2).then(function(response){
+            Item.adapter().expect('show', 'items', 2, {
+                result: [items[1]]
+            });
+            Item.show(2).then(function(response) {
                 var item = response.result;
-                
+
                 //Since the __iguana_type is 'item_type_2', the result is 
                 //an instance of ItemType2
                 expect(item.constructor).toBe(ItemType2);
             });
         });
-        
+
         // When a new instance is created, the \_\_iguana\_type is set automatically.
-        it('should create an instance of the class and set the __iguana_type if the __iguana_type is not set', function() { 
+        it('should create an instance of the class and set the __iguana_type if the __iguana_type is not set', function() {
             // ...
             var instance = ItemType1.new({});
             expect(instance.constructor).toBe(ItemType1);
             expect(instance.__iguana_type).toBe('item_type_1');
         });
-        
-        
+
+
         // Iguana classes cannot be created with 'new Item()'; You must call 'Item.new' 
         it('should only allow instances to be created with Item.new, not new Item()', function() {
             var func = function() {
@@ -95,36 +103,57 @@ describe('Iguana.SingleCollectionInheritance', function() {
             };
             expect(func).toThrow("Iguana classes must be instantiated with MyKlass.new() rather that new MyKlass()");
         });
-        
+
         it('should throw an error if no class matches the __iguana_type property', function() {
             // ...
             var func = function() {
-                Item.new({__iguana_type: 'noItemForThis'});
+                Item.new({
+                    __iguana_type: 'noItemForThis'
+                });
             };
             expect(func).toThrow('No class could be found for __iguana_type="noItemForThis".');
         });
-        
-        it('should not set the type property if there is no alias', function() { 
+
+        it('should not set the type property if there is no alias', function() {
             var UnAliasedItem = Iguana.subclass();
             var instance = UnAliasedItem.new({});
             expect(instance.constructor).toBe(UnAliasedItem);
             expect(instance.__iguana_type).toBeUndefined();
         });
-        
+
         it('should create an instance of the class if the type property matches the sciAlias', function() {
-            var instance = Item.new({__iguana_type: 'item'});
+            var instance = Item.new({
+                __iguana_type: 'item'
+            });
             expect(instance.constructor).toBe(Item);
         });
-        
+
+        it('should work with a lazy-loaded class', function() {
+            var mockClass = jasmine.createSpy('mockClass');
+            mockClass.inheritsFrom = function() {
+                return true;
+            };
+            mockClass.new = function() {
+                return new mockClass();
+            };
+
+            spyOn(Item, 'getAliasedKlass').andReturn(mockClass);
+            var instance = Item.new({
+                __iguana_type: 'lazy_loaded_item'
+            });
+            expect(Item.getAliasedKlass).toHaveBeenCalledWith('lazy_loaded_item');
+            expect(instance.constructor).toBe(mockClass);
+        });
+
     });
-    
+
     describe('sciProperty', function() {
-        
+
         it('should default to __iguana_type', function() {
             expect(Iguana.sciProperty).toBe('__iguana_type');
             expect(Iguana.subclass().sciProperty).toBe('__iguana_type');
         });
-        
+
         // ### Configuring the property used for the alias.
         // If you want to use something other than '\_\_iguana\_type' for
         // the property that defines the alias, you can use 'setSciProperty'.
@@ -134,35 +163,40 @@ describe('Iguana.SingleCollectionInheritance', function() {
                 this.setCollection('items');
                 this.setSciProperty('item_type');
             });
-            
+
             var ItemType1 = Item.subclass(function() {
                 this.alias('item_type_1');
             });
-            
+
             var ItemType2 = Item.subclass(function() {
                 this.alias('item_type_2');
             });
-            
+
             // Since we called setSciProperty('itemType'), we should
             // use the itemType property to define the alias of the 
             // class used to instantiate the document.
-            var item = {id: 1, item_type: 'item_type_1'};
-            
+            var item = {
+                id: 1,
+                item_type: 'item_type_1'
+            };
+
             //Mocking out the adapter to load up the item.
-            Item.adapter().expect('show', 'items', 1, {result: [item]});
-            Item.show(1).then(function(response){
+            Item.adapter().expect('show', 'items', 1, {
+                result: [item]
+            });
+            Item.show(1).then(function(response) {
                 var loadedItem = response.result;
-                
+
                 //Since the __iguana_type is 'item_type_1', the result is 
                 //an instance of ItemType1
                 expect(loadedItem.constructor).toBe(ItemType1);
             });
         });
-        
+
     });
-    
+
     describe('create', function() {
-        
+
         // ### create class method
         // Calling the create method on a subclass will
         // automatically set the appropriate class
@@ -171,18 +205,22 @@ describe('Iguana.SingleCollectionInheritance', function() {
                 this.alias('item');
                 this.setCollection('items');
             });
-            
+
             var ItemType1 = Item.subclass(function() {
                 this.alias('item_type_1');
             });
-            
-            var expectedObject = {'__iguana_type': 'item_type_1'}
-            ItemType1.adapter().expect('create', 'items', expectedObject, {result: [expectedObject]});
+
+            var expectedObject = {
+                '__iguana_type': 'item_type_1'
+            }
+            ItemType1.adapter().expect('create', 'items', expectedObject, {
+                result: [expectedObject]
+            });
             ItemType1.create({});
             ItemType1.adapter().flush('create');
         });
-        
+
     });
 
-    
+
 });
