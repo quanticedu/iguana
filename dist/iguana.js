df1053f40417e1468106b514276c38018c41fbc5
Nate Brustein <nate@pedago.com>
2014-12-17 12:26:56 -0500

    fix(setDefaultRequestOptions) make it accessible on provider

--- dist/iguana.js
+++ dist/iguana.js
@@ -11,6 +11,10 @@ angular.module('Iguana', ['SuperModel', 'ngResource'])
             this._defaultBaseUrl = baseUrl;
         };
 
+        this.setDefaultRequestOptions = function(opts) {
+            this._defaultRequestOptions = opts;
+        };
+
 
         this.$get = [
             'SuperModel',
@@ -78,6 +82,12 @@ angular.module('Iguana', ['SuperModel', 'ngResource'])
 
                 if (this._defaultAdapterName) Iguana.setAdapter(this._defaultAdapterName);
                 Iguana.setBaseUrl(this._defaultBaseUrl);
+
+                if (this._defaultRequestOptions) {
+                    angular.forEach(this._defaultRequestOptions, function(v, k) {
+                        Iguana.defaultRequestOptions().set(k, v);
+                    });
+                }
 
                 return Iguana;
             }
